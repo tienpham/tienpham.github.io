b11216568ca843912ab0c9b0d8ebad14efc4c68a
Tien Pham <tienph711@gmail.com>
2025-11-16 14:51:28 -0800

    update

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,10 @@
 export const SITE = {
+  author: "Reflexify",
   website: "https://reflexify.me/", // replace this with your deployed domain
   profile: "https://reflexify.me/",
   desc: "Reflexify.Me là không gian Thiền đương đại, nơi tri thức (RAG), hành động (Agentic AI) và sự tỉnh thức hội tụ. Những câu chuyện, góc nhìn và thực hành giúp kết nối mindfulness với công nghệ, mở ra cách thấy mới trong kỷ nguyên AI.",
   title: "Reflexify",
+  ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 4,
